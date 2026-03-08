ce7246b8e2f0c4798f82cd8388f81582dc3d8981
Chase Florell <chase.florell@gmail.com>
2026-03-08 10:00:07 -0700

    [CF] checkpoint

--- src/CodeToNeo4j/ProgramOptions/EnvironmentSetupHandler.cs
+++ src/CodeToNeo4j/ProgramOptions/EnvironmentSetupHandler.cs
@@ -2,19 +2,12 @@ using CodeToNeo4j.Graph;
 
 namespace CodeToNeo4j.ProgramOptions;
 
-public class EnvironmentSetupHandler : OptionsHandler
+public class EnvironmentSetupHandler(IGraphService graphService) : OptionsHandler
 {
-    public EnvironmentSetupHandler(IGraphService graphService)
-    {
-        _graphService = graphService;
-    }
-
     public override async Task Handle(Options options)
     {
-        await _graphService.Initialize(options.RepoKey, options.DatabaseName);
+        await graphService.Initialize(options.RepoKey, options.DatabaseName);
 
         await base.Handle(options);
     }
-
-    private readonly IGraphService _graphService;
 }
--- src/CodeToNeo4j/ProgramOptions/PurgeExecutionHandler.cs
+++ src/CodeToNeo4j/ProgramOptions/PurgeExecutionHandler.cs
@@ -2,22 +2,18 @@ using CodeToNeo4j.Graph;
 
 namespace CodeToNeo4j.ProgramOptions;
 
-public class PurgeExecutionHandler : OptionsHandler
+public class PurgeExecutionHandler(IGraphService graphService) : OptionsHandler
 {
-    public PurgeExecutionHandler(IGraphService graphService) => _graphService = graphService;
-
     public override async Task Handle(Options options)
     {
         if (options.PurgeData)
         {
             var allSupportedExtensions = new[] { ".cs", ".razor", ".xaml", ".js", ".html", ".xml", ".json", ".css", ".csproj" };
             var includeExtensions = options.IncludeExtensions.SequenceEqual(allSupportedExtensions) ? null : options.IncludeExtensions;
-            await _graphService.PurgeData(options.RepoKey, includeExtensions, options.DatabaseName);
+            await graphService.PurgeData(options.RepoKey, includeExtensions, options.DatabaseName);
             return; // Terminate chain after purge
         }
 
         await base.Handle(options);
     }
-
-    private readonly IGraphService _graphService;
 }
